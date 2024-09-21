c8b132e75f228b7aa0afe73905fae9e9652906b8
Anmol Chawla <anmolchawla10@gmail.com>
2024-09-21 18:54:06 +0530

    Add a navigation bar for the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,37 @@
 <script>
 	import '../app.css';
+
+	/**
+	 * Map of page name to path
+	 *
+	 * @type {{name: string, path: string}[]}
+	 */
+	var locationMap = [
+		{ name: 'Home', path: '/' },
+		{ name: 'Blog', path: '/blog' },
+		{ name: 'Projects', path: '/projects' }
+	];
 </script>
 
+<div class="navigation-bar">
+	{#each locationMap as { name, path }, index}
+		<div>
+			<a href={path}>{name}</a>
+		</div>
+		{#if index + 1 != locationMap.length}
+			<div>
+				<p>|</p>
+			</div>
+		{/if}
+	{/each}
+</div>
+
 <slot />
+
+<style>
+	.navigation-bar {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+</style>
